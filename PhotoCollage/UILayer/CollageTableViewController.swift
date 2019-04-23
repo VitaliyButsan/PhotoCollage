b1d1cf7514fe48a9f8304782e4f3cd18e2a3acfc
vitket <skolkojvastut@gmail.com>
2019-04-23 16:04:55 +0300

    1) Add Pagination 2) Add Pull-to-refresh 3) Add Alert

--- PhotoCollage/UILayer/CollageTableViewController.swift
+++ PhotoCollage/UILayer/CollageTableViewController.swift
@@ -11,36 +11,73 @@ import Netvit
 
 class CollageTableViewController: UITableViewController {
 
-    let photoStore = PhotoStorage()
+    private let photoStoreClass = PhotoStorage()
+    private var pageNumber = 1
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        // call refresh control
+        refreshControl()
         // call getPhoto func
         getPhoto()
     }
     
     // define getPhoto func
-    private func getPhoto() {
-        Netvit.getPhotos(onPage: 1, withLimit: 5) { result in
-            switch result {
-            case .success(let photos):
-                self.photoStore.storagePhoto = photos
-                self.tableView.reloadData()
-            case .failure(let error):
-                print(error)
+    @objc private func getPhoto() {
+        if pageNumber <= 10 {
+            Netvit.getPhotos(onPage: pageNumber, withLimit: 30) { result in
+                switch result {
+                case .success(let receivedPhotos):
+                    self.photoStoreClass.storagePhoto += receivedPhotos
+                    self.tableView.reloadData()
+                case .failure( _ ):
+                    // if data not received
+                    self.alert(withTitle: "Data not received!", withMessage: "Try pull down to refresh.", titleForActionButton: "Ok")
+                    self.tableView.reloadData()
+                }
+                // stop refresh control
+                self.tableView.refreshControl?.endRefreshing()
             }
+            
+            pageNumber += 1
+            
+        } else {
+            // if pages is over
+            alert(withTitle: "No more photos!", withMessage: "", titleForActionButton: "Ok")
         }
     }
 
+    // set up pull to refresh func
+    private func refreshControl() {
+        photoStoreClass.clearStoragePhoto()
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
+        refreshControl.addTarget(self, action: #selector(getPhoto), for: UIControl.Event.valueChanged)
+        tableView.refreshControl = refreshControl
+    }
+    
+    // set up alert handler
+    private func alert(withTitle title: String, withMessage message: String, titleForActionButton buttonTitle: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+        
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photoStore.storagePhoto.count
+        return photoStoreClass.storagePhoto.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = "\(indexPath.row)"
-
+        
+        // pagination cells
+        if indexPath.row == photoStoreClass.storagePhoto.count - 1 {
+            getPhoto()
+        }
+        
         return cell
     }
 
